3e34016435da94c6fc83a1ed42fc811e6410ce80
helenqu <helenqu@seas.upenn.edu>
2015-02-16 22:32:37 -0500

    switched to postgresql

--- hw04/hw04app/db/schema.rb
+++ hw04/hw04app/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150216185204) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: true do |t|
     t.string   "user"
     t.float    "lat"
