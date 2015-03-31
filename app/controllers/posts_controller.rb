10ef20e19d4a8a28c488266999e79e878933ebcd
johnathan-zeng <zeng.johnathan@gmail.com>
2015-03-31 12:22:42 -0400

    bugs for hw8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,9 +58,9 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to users_path
+    # @post = Post.find(params[:id])
+    # @post.destroy
+    # redirect_to users_path
 
     # respond_to do |format|
     #   format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
